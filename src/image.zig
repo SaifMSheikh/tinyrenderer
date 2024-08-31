46facf76fa408e7f06fde8567b3e74d6daeca5c9
saif <saifmsheikh@gmail.com>
2024-08-31 22:21:13 +0500

    Implemented Barycentric Triangle Rendering

--- src/image.zig
+++ src/image.zig
@@ -7,47 +7,72 @@ pub const Image=struct{
 //Types
     const Color=[3]u8;
 //Draw Functions
-    pub fn getColor(self:*Image,x:u64,y:u64)Color{
-        var offset=3*(x+y*self.width);
-        while(offset+3>=3*self.height*self.width)
-            offset-=1;
-        return .{
-            self.pdata.ptr[0+offset],
-            self.pdata.ptr[1+offset],
-            self.pdata.ptr[2+offset]
-        };
-    }
     pub fn setColor(self:*Image,x:u64,y:u64,color:Color)void{
         var offset=3*(x+y*self.width);
         while(offset+3>=3*self.height*self.width)
             offset-=1;
         for(0..3)|i|{self.pdata.ptr[offset+i]=color[i];}
     }
-    pub fn drawLine(self:*Image,x0:*i64,y0:*i64,x1:*i64,y1:*i64,color:Color)void{
+    pub fn drawLine(self:*Image,a0:i64,b0:i64,a1:i64,b1:i64,color:Color)void{
         //Order Endpoints
-        const transpose:bool=@abs(x1.*-x0.*)<@abs(y1.*-y0.*);
-        if(transpose){
-            std.mem.swap(i64,x0,y0);
-            std.mem.swap(i64,x1,y1);
-        }
-        if(x0.*>x1.*){
-            std.mem.swap(i64,x0,x1);
-            std.mem.swap(i64,y0,y1);
+        const transpose:bool=@abs(a1-a0)<@abs(b1-b0);
+        var x0=if(transpose)b0 else a0;
+        var y0=if(transpose)a0 else b0;
+        var x1=if(transpose)b1 else a1;
+        var y1=if(transpose)a1 else b1;
+        if(x0>x1){
+            var tmp:i64=undefined;
+            tmp=x0;x0=x1;x1=tmp;
+            tmp=y0;y0=y1;y1=tmp;
         }
         //Clamp Endpoints
-        x0.*=std.math.clamp(x0.*,0,@as(i64,@intCast(self.width-1)));
-        x1.*=std.math.clamp(x1.*,0,@as(i64,@intCast(self.width-1)));
-        y0.*=std.math.clamp(y0.*,0,@as(i64,@intCast(self.height)));
-        y1.*=std.math.clamp(y1.*,0,@as(i64,@intCast(self.height)));
+        x0=std.math.clamp(x0,0,@as(i64,@intCast(self.width-1)));
+        x1=std.math.clamp(x1,0,@as(i64,@intCast(self.width-1)));
+        y0=std.math.clamp(y0,0,@as(i64,@intCast(self.height)));
+        y1=std.math.clamp(y1,0,@as(i64,@intCast(self.height)));
         //Linearly Interpolate A Line
         var y:f32=undefined;
         var t:f32=undefined;
-        for(@intCast(x0.*)..@intCast(x1.*))|x|{
-            t=@as(f32,@floatFromInt(@as(i64,@intCast(x))-x0.*))/@as(f32,@floatFromInt(x1.*-x0.*));
-            y=@as(f32,@floatFromInt(y0.*))*(1.0-t)+@as(f32,@floatFromInt(y1.*))*t;
+        for(@intCast(x0)..@intCast(x1))|x|{
+            t=@as(f32,@floatFromInt(@as(i64,@intCast(x))-x0))/@as(f32,@floatFromInt(x1-x0));
+            y=@as(f32,@floatFromInt(y0))*(1.0-t)+@as(f32,@floatFromInt(y1))*t;
             if(transpose)
                 {self.setColor(@intFromFloat(y),x,color);}
-            else self.setColor(x,@intFromFloat(y),color);
+            else{self.setColor(x,@intFromFloat(y),color);}
+        }
+    }
+    pub fn drawTriangle(self:*Image,a0:f32,b0:f32,a1:f32,b1:f32,a2:f32,b2:f32,color:[3]u8)void{
+        //Clamp Vertices
+        const x0:f32=std.math.clamp(a0,0.0,@as(f32,@floatFromInt(self.width)));
+        const x1:f32=std.math.clamp(a1,0.0,@as(f32,@floatFromInt(self.width)));
+        const x2:f32=std.math.clamp(a2,0.0,@as(f32,@floatFromInt(self.width)));
+        const y0:f32=std.math.clamp(b0,0.0,@as(f32,@floatFromInt(self.height)));
+        const y1:f32=std.math.clamp(b1,0.0,@as(f32,@floatFromInt(self.height)));
+        const y2:f32=std.math.clamp(b2,0.0,@as(f32,@floatFromInt(self.height)));
+        if(y0==y1 and y0==y2)return;
+        //Compute Bounding Box
+        var xmin=x0;
+        if(xmin>x1)xmin=x1;
+        if(xmin>x2)xmin=x2;
+        var xmax=x0;
+        if(xmax<x1)xmax=x1;
+        if(xmax<x2)xmax=x2;
+        var ymin=y0;
+        if(ymin>y1)ymin=y1;
+        if(ymin>y2)ymin=y2;
+        var ymax=y0;
+        if(ymax<y1)ymax=y1;
+        if(ymax<y2)ymax=y2;
+        //Test Pixels
+        var x:f32=xmin;
+        while(x<=xmax):(x+=0.5){
+            var y:f32=ymin;
+            while(y<=ymax):(y+=0.5){
+                const bc=barycentric(x0,y0,x1,y1,x2,y2,x,y);
+                if(bc[0]<0.0 or bc[1]<0.0 or bc[2]<0.0){continue;}
+                else if(bc[0]>1.0 or bc[1]>1.0 or bc[2]>1.0){continue;}
+                else{self.setColor(@intFromFloat(x),@intFromFloat(y),color);}
+            }
         }
     }
 //Edit Functions
@@ -60,5 +85,39 @@ pub const Image=struct{
                 self.setColor(x,self.height-y,tmp_color);
             }
         }
+    }
+//Utility Functions
+    pub fn getColor(self:*Image,x:u64,y:u64)Color{
+        var offset=3*(x+y*self.width);
+        while(offset+3>=3*self.height*self.width)
+            offset-=1;
+        return .{
+            self.pdata.ptr[0+offset],
+            self.pdata.ptr[1+offset],
+            self.pdata.ptr[2+offset]
+        };
+    }
+    fn barycentric(x0:f32,y0:f32,x1:f32,y1:f32,x2:f32,y2:f32,px:f32,py:f32)[3]f32{
+        const a:[3]f32=.{
+            x2-x0,
+            x1-x0,
+            x0-px
+        };
+        const b:[3]f32=.{
+            y2-y0,
+            y1-y0,
+            y0-py
+        };
+        const c:[3]f32=.{
+            a[1]*b[2]-a[2]*b[1],
+            a[2]*b[0]-a[0]*b[2],
+            a[0]*b[1]-a[1]*b[0]
+        };
+        if(@abs(c[2])<1)return.{-1.0,-1.0,-1.0};
+        return.{
+            1.0-(c[0]+c[1])/c[2],
+            c[1]/c[2],
+            c[0]/c[2]
+        };
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,7 @@ pub fn main()!void{
     //Load Model Data From Input File
     var arena_allocator=std.heap.ArenaAllocator.init(allocator);
     defer arena_allocator.deinit();
-    var model=try Model.init("obj/african_head.obj",arena_allocator.allocator());
+    var model=try Model.init("obj/teapot.obj",arena_allocator.allocator());
     defer model.deinit();
     //Open Output File
     const file=try std.fs.cwd().createFile("img.tga",.{.read=true});
@@ -34,16 +34,28 @@ pub fn main()!void{
     //Draw
     std.debug.print("Processing {} vertices...",.{model.verts.items.len});
     for(model.faces.items)|face|{
-        for(0..3)|i|{
-            const v0=model.verts.items[face[i]-1];
-            const v1=model.verts.items[face[(i+1)%3]-1];
-            var x0:i64=@intFromFloat((v0[0]+1.0)*width/2.0);
-            var y0:i64=@intFromFloat((v0[1]+1.0)*height/2.0);
-            var x1:i64=@intFromFloat((v1[0]+1.0)*width/2.0);
-            var y1:i64=@intFromFloat((v1[1]+1.0)*height/2.0);
-            img.drawLine(&x0,&y0,&x1,&y1,.{255}**3);
-        }
+        const v0=model.verts.items[face[0]-1];
+        const v1=model.verts.items[face[1]-1];
+        const v2=model.verts.items[face[2]-1];
+        const x0:f32=(v0[0]+1.0)*width/2.0;
+        const x1:f32=(v1[0]+1.0)*width/2.0;
+        const x2:f32=(v2[0]+1.0)*width/2.0;
+        const y0:f32=(v0[1]+1.0)*height/2.0;
+        const y1:f32=(v1[1]+1.0)*height/2.0;
+        const y2:f32=(v2[1]+1.0)*height/2.0;
+        img.drawTriangle(x0,y0,x1,y1,x2,y2,.{255}**3);
     }
+//    for(model.faces.items)|face|{
+//        for(0..3)|i|{
+//            const v0=model.verts.items[face[i]-1];
+//            const v1=model.verts.items[face[(i+1)%3]-1];
+//            const x0:i64=@intFromFloat((v0[0]+1.0)*width/2.0);
+//            const y0:i64=@intFromFloat((v0[1]+1.0)*height/2.0);
+//            const x1:i64=@intFromFloat((v1[0]+1.0)*width/2.0);
+//            const y1:i64=@intFromFloat((v1[1]+1.0)*height/2.0);
+//            img.drawLine(x0,y0,x1,y1,.{255}**3);
+//        }
+//    }
     std.debug.print("Done.\n",.{});
     //Write To File
     img.flip_vertical();
